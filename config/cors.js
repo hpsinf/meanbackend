a976781829a0b30f500592010ab5a1285c2a2641
Henrique Pinheiro da Silva <hps.inf@gmail.com>
2020-09-29 20:27:35 -0300

    akuste cors  inicio impl autenticação

--- config/cors.js
+++ config/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATH")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization")
     next()
 }
