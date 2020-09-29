841717dc549f5ca1540d5d80708ca804fec4c58e
Henrique Pinheiro da Silva <hps.inf@gmail.com>
2020-09-29 16:03:19 -0300

    imp midware queriparser no server

--- config/server.js
+++ config/server.js
@@ -1,9 +1,8 @@
 const port = process.env.PORT || 3003 
 const msg = require('./msgPT_BR')
-
+const queryParser = require('express-query-int')
 //body-parser  middleware responsável pelo parser nas resquições
 const bodyParser = require('body-parser')
-
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
@@ -12,6 +11,7 @@ msg()
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, ()=>{
     console.log(`BACKEND na porta ${port}.`)
